9dacc92a34f7bfeaf86889ac8a39e7c5b35f0883
Kentaro Hayashi <kenhys@gmail.com>
2018-08-14 15:56:47 +0900

    Add radio menu sample

---
+++ radio-menu/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#set -x
+case $1 in
+    debug)
+        G_MESSAGES_DEBUG="all" ./ibus-engine-radiomenu 2>&1 | tee test.log
+        ;;
+    *)
+        ./ibus-engine-radiomenu 2>&1 | tee test.log
+        ;;
+esac
