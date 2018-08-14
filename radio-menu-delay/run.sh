577b49a3d41756dd01677a42d11ad4478a6b0352
Kentaro Hayashi <kenhys@gmail.com>
2018-08-14 16:47:54 +0900

    Fix a typo

--- radio-menu-delay/run.sh
+++ radio-menu-delay/run.sh
@@ -3,9 +3,9 @@
 #set -x
 case $1 in
     debug)
-        G_MESSAGES_DEBUG="all" ./ibus-engine-radiomenu 2>&1 | tee test.log
+        G_MESSAGES_DEBUG="all" ./ibus-engine-radiomenudelay 2>&1 | tee test.log
         ;;
     *)
-        ./ibus-engine-radiomenu 2>&1 | tee test.log
+        ./ibus-engine-radiomenudelay 2>&1 | tee test.log
         ;;
 esac
