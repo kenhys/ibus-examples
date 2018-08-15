eab6085395e7ccef09c4dfe37e547599d4d4ead5
Kentaro Hayashi <kenhys@gmail.com>
2018-08-15 16:39:50 +0900

    Fix a typo

--- radio-menu-from-array/run.sh
+++ radio-menu-from-array/run.sh
@@ -3,9 +3,9 @@
 #set -x
 case $1 in
     debug)
-        G_MESSAGES_DEBUG="all" ./ibus-engine-radiomenuarray 2>&1 | tee test.log
+        G_MESSAGES_DEBUG="all" ./ibus-engine-radiomenufromarray 2>&1 | tee test.log
         ;;
     *)
-        ./ibus-engine-radiomenuarray 2>&1 | tee test.log
+        ./ibus-engine-radiomenufromarray 2>&1 | tee test.log
         ;;
 esac
