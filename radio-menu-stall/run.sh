437e15e1fcf5cf5a1c3e01ac17a05167753ef28f
Kentaro Hayashi <kenhys@gmail.com>
2018-08-15 11:54:44 +0900

    Fix program name

--- radio-menu-stall/run.sh
+++ radio-menu-stall/run.sh
@@ -3,9 +3,9 @@
 #set -x
 case $1 in
     debug)
-        G_MESSAGES_DEBUG="all" ./radiomenustall 2>&1 | tee test.log
+        G_MESSAGES_DEBUG="all" ./ibus-engine-radiomenustall 2>&1 | tee test.log
         ;;
     *)
-        ./radiomenustall 2>&1 | tee test.log
+        ./ibus-engine-radiomenustall 2>&1 | tee test.log
         ;;
 esac
