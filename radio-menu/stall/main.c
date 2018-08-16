07507dcd63860b5200b3ad4051c883e69328d109
Kentaro Hayashi <kenhys@gmail.com>
2018-08-16 23:37:50 +0900

    Simplify

--- radio-menu/stall/main.c
+++ radio-menu/stall/main.c
@@ -1,20 +1,20 @@
 #include <ibus.h>
 #include <glib.h>
 
-typedef struct _IBusRadiomenuStallEngine IBusRadiomenuStallEngine;
-typedef struct _IBusRadiomenuStallEngineClass IBusRadiomenuStallEngineClass;
+typedef struct _IBusSampleEngine IBusSampleEngine;
+typedef struct _IBusSampleEngineClass IBusSampleEngineClass;
 
-struct _IBusRadiomenuStallEngine {
+struct _IBusSampleEngine {
   IBusEngine parent;
 };
 
-struct _IBusRadiomenuStallEngineClass {
+struct _IBusSampleEngineClass {
   IBusEngineClass parent;
 };
 
-static void ibus_radiomenustall_engine_class_init(IBusRadiomenuStallEngineClass	*klass);
-static void ibus_radiomenustall_engine_init(IBusRadiomenuStallEngine *engine);
-static void ibus_radiomenustall_engine_destroy(IBusRadiomenuStallEngine *engine);
+static void ibus_sample_engine_class_init(IBusSampleEngineClass	*klass);
+static void ibus_sample_engine_init(IBusSampleEngine *engine);
+static void ibus_sample_engine_destroy(IBusSampleEngine *engine);
 
 static void focus_in(IBusEngine *engine);
 static void focus_out(IBusEngine *engine);
@@ -25,20 +25,20 @@ static void property_activate(IBusEngine *engine,
                               const gchar *prop_name,
                               guint prop_state);
 
-G_DEFINE_TYPE(IBusRadiomenuStallEngine, ibus_radiomenustall_engine, IBUS_TYPE_ENGINE)
+G_DEFINE_TYPE(IBusSampleEngine, ibus_sample_engine, IBUS_TYPE_ENGINE)
 
-#define IBUS_TYPE_RADIOMENUSTALL_ENGINE (ibus_radiomenustall_engine_get_type())
+#define IBUS_TYPE_SAMPLE_ENGINE (ibus_sample_engine_get_type())
 
-GType ibus_radiomenustall_engine_get_type(void);
+GType ibus_sample_engine_get_type(void);
 
 static void
-ibus_radiomenustall_engine_class_init(IBusRadiomenuStallEngineClass *klass)
+ibus_sample_engine_class_init(IBusSampleEngineClass *klass)
 {
   g_debug(G_STRFUNC);
   IBusObjectClass *ibus_object_class = IBUS_OBJECT_CLASS (klass);
   IBusEngineClass *engine_class = IBUS_ENGINE_CLASS (klass);
 
-  ibus_object_class->destroy = (IBusObjectDestroyFunc)ibus_radiomenustall_engine_destroy;
+  ibus_object_class->destroy = (IBusObjectDestroyFunc)ibus_sample_engine_destroy;
 
   engine_class->enable = enable;
   engine_class->disable = disable;
@@ -54,7 +54,7 @@ static IBusProperty *menua = NULL;
 static IBusProperty *menub = NULL;
 
 static void
-ibus_radiomenustall_engine_init(IBusRadiomenuStallEngine *engine)
+ibus_sample_engine_init(IBusSampleEngine *engine)
 {
   g_debug(G_STRFUNC);
   IBusText *label = ibus_text_new_from_static_string("Radio Menu Stall");
@@ -116,7 +116,7 @@ ibus_radiomenustall_engine_init(IBusRadiomenuStallEngine *engine)
 }
 
 static void
-ibus_radiomenustall_engine_destroy(IBusRadiomenuStallEngine *engine)
+ibus_sample_engine_destroy(IBusSampleEngine *engine)
 {
   g_debug(G_STRFUNC);
   if (menu) {
@@ -129,11 +129,11 @@ ibus_radiomenustall_engine_destroy(IBusRadiomenuStallEngine *engine)
   }
   if (menua) {
     g_object_unref(menua);
-    root = NULL;
+    menua = NULL;
   }
   if (menub) {
     g_object_unref(menub);
-    root = NULL;
+    menub = NULL;
   }
   ((IBusObjectClass *)ibus_radiomenustall_engine_parent_class)->destroy((IBusObject *)engine);
 }
@@ -235,7 +235,7 @@ int main(int argc, char *argv[])
 
   IBusFactory *factory = ibus_factory_new(ibus_bus_get_connection(bus));
   g_object_ref_sink(factory);
-  ibus_factory_add_engine(factory, "radiomenustall", IBUS_TYPE_RADIOMENUSTALL_ENGINE);
+  ibus_factory_add_engine(factory, "radiomenustall", IBUS_TYPE_SAMPLE_ENGINE);
 
   if (execute_by_ibus) {
     ibus_bus_request_name(bus, "com.example.IBus.RADIOMENUSTALL", 0);
